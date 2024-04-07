cf86c6ca0134a838ca80aaf6942984d8e7224751
cpin <37347272+CorentinPineault@users.noreply.github.com>
2024-04-07 23:18:26 +0200

    Update WorkshopDLWarehouseWorker.py
    Resolved encoding issues
    Resolved invalid directory name error due to special characters
    Changed function for finding model name (select_one -> find) for better name recovery
    Added try catch in case of no main preview

--- WorkshopDLWarehouseWorker.py
+++ WorkshopDLWarehouseWorker.py
@@ -1,10 +1,12 @@
 import os
 import requests
+import re
+
 from zipfile import ZipFile
 from bs4 import BeautifulSoup
 
-modelDLFolder = "./steamcmd/steamapps/workshop/content/1840/"
-modelFinalFolder = "Models/"
+modelDLFolder = ".\\steamcmd\\steamapps\\workshop\\content\\1840\\"
+modelFinalFolder = ".\\Models\\"
 
 if not os.path.isdir(modelFinalFolder):
     os.mkdir(modelFinalFolder)
@@ -15,28 +17,44 @@ for (paths, dirs, filenames) in w:
         if name.endswith('.zip'):
             # Find workshop page from id
             wsID = filenames[0][0:-4]
+            print("Charging {}...".format(wsID))
             URL = "https://steamcommunity.com/sharedfiles/filedetails/?id={}".format(wsID)
             page = requests.get(URL)
             soup = BeautifulSoup(page.content, "html.parser")
-            newName = soup.select_one(".workshopItemTitle").get_text()
-            print(wsID + ' ' + newName)
+            newName = soup.find(attrs={"class": "workshopItemTitle"}).get_text()
+            print("Model : {}".format(newName))
 
             # Get link of preview image
-            imgPreview = requests.get(soup.find(attrs={"id": "previewImageMain"})['src'])
 
+            try:
+                imgPreview = requests.get(soup.find(attrs={"id": "previewImageMain"})['src'])
+            except TypeError:
+                print("Main preview image not available, selecting the worskshop image instead...")
+                imgPreview = requests.get(soup.find(attrs={"id": "previewImage"})['src'])
+                      
             # Extract model into models folder
-            zippy = ZipFile(modelDLFolder + wsID + "/" + wsID + ".zip","r")
-            zippy.extractall(modelFinalFolder + newName)
-
+            print(r"Starting extraction from {}{}\\{}.zip".format(modelDLFolder,wsID,wsID))
+            newNamePath = re.sub(r'[^\w_. -]', '', newName)
+            
+            with ZipFile(r"{}{}\\{}.zip".format(modelDLFolder,wsID,wsID)) as zippy:
+                # zippy.printdir()
+                zippy.extractall(r'{}{}'.format(modelFinalFolder, newNamePath))
+            zippy.close()
+            
+            print(r'Extraction to {}{} complete !'.format(modelFinalFolder, newNamePath))
+                  
             # Download preview image
-            with open(modelFinalFolder + newName + "/" + newName + ".jpg", "wb") as f:
+            with open(modelFinalFolder + newNamePath + "\\" + newNamePath + ".jpg", "wb") as f:
                 f.write(imgPreview.content)
 
             # Take description and steam user names in place in a file
             textDesc = soup.find(attrs={"class": "workshopItemDescription"}).get_text()
             # Find the usename account box, stript the tabs, take the username from the raw code by split at the newline
             creatorName = soup.find(attrs={"class": "friendBlockContent"}).get_text().strip().rsplit("\n")[0]
-        
+ 
+            print("Uploaded by : {}. Getting description... ".format(creatorName))
+            
+            with open("{}{}\\readme.txt".format(modelFinalFolder, newNamePath), 'w', encoding="utf-8") as f:
+                f.write('{} (ID {})\nDownloaded from Steam, uploaded by {}.\n\nOriginal description : \n{}'.format(newName, wsID, creatorName, textDesc))
 
-            with open(modelFinalFolder + newName + "/" +'readme.txt', 'w') as f:
-                f.write('Downloaded from Steam, from {}. \n Original description : \n{}'.format(creatorName, textDesc))
+            print("{} successfully moved ! \n".format(newName))
